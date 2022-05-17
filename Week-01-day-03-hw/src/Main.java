2d8ed664657fa32ef1cf99d0a4d6844acb00725e
aalsu <aalsulami35@gmail.com>
2022-05-17 23:02:18 +0300

    HomeWork Day 3

--- Week-01-day-03-hw/src/Main.java
+++ Week-01-day-03-hw/src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //1
+        div(1,0);
+
+        //2
+        try {
+            calculateTax(100);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        //3
+        Circle circle = new Circle(3.0);
+        System.out.println("the radius of "+circle.getRadius()+" and area = "+circle.getArea());
+
+        //4
+        Customer customer = new Customer(1,"ahmed",'m');
+        System.out.println(customer);
+
+        //5
+
+        PolyLine l1 = new PolyLine();
+
+        l1.appendPoint(new MyPoint(2, 4));
+        l1.appendPoint(3, 4);
+        System.out.println(l1);
+
+        // Test constructor 2
+        List<MyPoint> points = new ArrayList<MyPoint>();
+        points.add(new MyPoint(11, 12));
+        PolyLine l2 = new PolyLine(points);
+        System.out.println(l2);
+    }
+
+
+    public static void div(int num1, int num2) throws Exception{
+
+        if (num2 == 0){
+            throw new Exception("Can't div by 0");
+        }
+        System.out.println(num1/num2);
+    }
+    public static void calculateTax(int price)throws Exception{
+
+        if(price<100 || price>500){
+            throw new Exception("Price out of range");
+
+        }
+        System.out.println("The Tax = "+price*15/100);
     }
 }
